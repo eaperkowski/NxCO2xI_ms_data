fec3f38c6c54f6ef92ec38c0551368ce55de7ed4
eaperkowski <evan.a.perkowski@ttu.edu>
2023-11-17 16:11:39 -0600

    annotate and clean up assess chamber climate script

--- analyses/NxCO2xI_assess_chamber_climate.R
+++ analyses/NxCO2xI_assess_chamber_climate.R
@@ -1,11 +1,15 @@
-## Load libraries
+
+##########################################################################
+## Load libraries and data files
+##########################################################################
 library(dplyr)
 library(lubridate)
 library(tidyverse)
 library(ggpubr)
+library(emmeans)
 
 ## Read in chamber climate data
-chamber.data <- list.files(path = "../chamber_climate",
+chamber.data <- list.files(path = "../chamber_climate/machine_readout/",
            pattern = "\\.csv$", recursive = TRUE,
            full.names = TRUE)
 chamber.data <- setNames(chamber.data, chamber.data)
@@ -27,14 +31,15 @@ df <- chamber.clim %>%
          month = month(date),
          month.day = str_c(month, "/", day))
 
+## Check single day CO2
 ggplot(data = subset(df, chamber == "1" & month.day == "7/10"),
        aes(x = date, y = co2.meas)) +
   geom_line()
 
-############ 
-## Read in HOBO sensor temp data 
-############
-hobo.data <- list.files(path = "../hobo_data",
+##########################################################################
+## Read HOBO sensor temp data 
+##########################################################################
+hobo.data <- list.files(path = "../chamber_climate/hobo/",
                            pattern = "\\.csv$", recursive = TRUE,
                            full.names = TRUE)
 hobo.data <- setNames(hobo.data, hobo.data)
@@ -54,126 +59,22 @@ hobo.clim <- lapply(hobo.data, read.csv) %>%
                                   21))) %>%
   dplyr::select(chamber, date.time, tod, air.temp, temp.set)
 
-
+##########################################################################
+## Subset hobo data into eCO2 and aCO2 date ranges
+##########################################################################
 eco2.hobo <- hobo.clim %>% 
-  pivot_longer(cols = air.temp:temp.set, 
-               names_to = "temp_type", 
-               values_to = "temp") %>% 
   filter(date.time > "2022-06-18" & date.time < "2022-08-05")  %>%
   mutate(co2.treat = "elevated")
 
 aco2.hobo <- hobo.clim %>% 
-  pivot_longer(cols = air.temp:temp.set, 
-               names_to = "temp_type", 
-               values_to = "temp") %>%
   filter(date.time > "2022-08-14" & date.time < "2022-10-01") %>%
   mutate(co2.treat = "ambient")
 
 
-eco2.plot <- ggplot(data = subset(eco2.hobo, chamber == 6)) +
-  geom_line(aes(x = date.time, y = temp, color = temp_type), 
-            linewidth = 1) +
-  scale_color_manual(values = c("red", "blue"),
-                     labels = c("HOBO readout",
-                                "Set point")) +
-  scale_y_continuous(limits = c(0, 32), breaks = seq(0, 30, 10)) +
-  theme_bw(base_size = 18) +
-  labs(x = "Date", y = expression("Air temperature ("*degree*"C)"),
-       color = NULL)
-  
-png("../NxCO2xI_sample_chamber_climate.png", 
-    width = 10, height = 4,
-    units = "in", res = 600)
-ggplot(data = subset(aco2.hobo, chamber == 6)) +
-  geom_line(aes(x = date.time, y = temp, color = temp_type), 
-            linewidth = 1) +
-  scale_color_manual(values = c("red", "blue"),
-                     labels = c("HOBO readout",
-                                "Set point")) +
-  scale_y_continuous(limits = c(0, 32), breaks = seq(0, 30, 10)) +
-  theme_bw(base_size = 18) +
-  labs(x = "Date", y = expression("Air temperature ("*degree*"C)"),
-       color = NULL)
-dev.off()
-
-
-png("../NxCO2xI_sample_chamber_climate_7days.png", 
-    width = 10, height = 4,
-    units = "in", res = 600)
-aco2.hobo %>%
-  filter(date.time > "2022-09-01" & date.time < "2022-09-08" & chamber == 6)  %>%
-ggplot() +
-  geom_line(aes(x = date.time, y = temp, color = temp_type), 
-            linewidth = 1) +
-  scale_color_manual(values = c("red", "blue"),
-                     labels = c("HOBO readout",
-                                "Set point")) +
-  scale_y_continuous(limits = c(0, 32), breaks = seq(0, 30, 10)) +
-  theme_bw(base_size = 18) +
-  labs(x = "Date", y = expression("Air temperature ("*degree*"C)"),
-       color = NULL)
-dev.off()
-
-
-
-ggarrange(eco2.plot, aco2.plot, ncol = 2, 
-          common.legend = TRUE, legend = "right")
-
-
-
-
-eco2.hobo.all <- eco2.hobo %>%
-  group_by(date.time) %>%
-  summarize(temp.mean = mean(air.temp),
-            temp.sd = sd(air.temp))
-
-aco2.hobo.all <- aco2.hobo %>%
-  group_by(date.time) %>%
-  summarize(temp.mean = mean(air.temp),
-            temp.sd = sd(air.temp))
-
-
-ggplot() +
-  geom_line(data = eco2.hobo.all, 
-            aes(x = date.time, y = temp.mean),
-            size = 2) +
-  geom_ribbon(data = eco2.hobo.all,
-              aes(x = date.time, 
-                  ymin = temp.mean - temp.sd,
-                  ymax = temp.mean + temp.sd))
-  
-  
-eco2.plot <- ggplot() +
-  geom_line(data = eco2.hobo, 
-            aes(x = date.time, y = air.temp, 
-                color = factor(chamber))) +
-  geom_line(data = eco2.hobo.all, 
-            aes(x = date.time, y = temp.mean),
-            size = 2) +
-  scale_y_continuous(limits = c(0, 30, 5)) +
-  scale_color_brewer(palette = "Spectral") +
-  theme_bw()
-
-aco2.plot <- ggplot() +
-  geom_line(data = aco2.hobo, 
-            aes(x = date.time, y = air.temp, 
-                color = factor(chamber))) +
-  scale_y_continuous(limits = c(0, 30, 5)) +
-  geom_line(data = aco2.hobo.all, 
-            aes(x = date.time, y = temp.mean),
-            size = 2) +
-  scale_color_brewer(palette = "Spectral") +
-  theme_bw()
-
-
-
-ggarrange(aco2.plot, eco2.plot, ncol = 2, common.legend = TRUE)
-
-
-#############
+##########################################################################
 ## Filter chamber.clim to dates of experiment, tack on 
 ## whether this was for elevated or ambient CO2 iteration
-#############
+##########################################################################
 eco2 <- chamber.clim %>%
   filter(date > "2022-06-18" & date < "2022-08-05") %>%
   mutate(co2.treat = "elevated")
@@ -181,6 +82,9 @@ aco2 <- chamber.clim %>%
   filter(date > "2022-08-14" & date < "2022-10-01") %>%
   mutate(co2.treat = "ambient")
 
+##########################################################################
+## Summarize night temperature and relative humidity data across chambers
+##########################################################################
 trt.night.means <- eco2 %>% full_join(aco2) %>%
   filter(temp.set == 17) %>%
   group_by(co2.treat, chamber) %>%
@@ -192,6 +96,9 @@ trt.night.means <- eco2 %>% full_join(aco2) %>%
             rh.mean = mean(rh),
             rh.sd = sd(rh))
 
+##########################################################################
+## Summarize day temperature and relative humidity data across chambers
+##########################################################################
 trt.day.means <- eco2 %>% full_join(aco2) %>%
   filter(temp.set != 17) %>%
   group_by(co2.treat, chamber) %>%
@@ -203,26 +110,17 @@ trt.day.means <- eco2 %>% full_join(aco2) %>%
             rh.mean = mean(rh),
             rh.sd = sd(rh))
 
-trt.all.means <- eco2 %>% full_join(aco2) %>%
-  group_by(co2.treat, chamber) %>%
-  summarize(temp = mean(temp.meas),
-            rh = mean(humidity.meas)) %>%
-  ungroup(chamber) %>%
-  summarize(temp.mean = mean(temp),
-            temp.sd = sd(temp),
-            rh.mean = mean(rh),
-            rh.sd = sd(rh))
-
-
-co2.variability <- eco2 %>% full_join(aco2) %>%
+##########################################################################
+## Summarize CO2 data across chambers
+##########################################################################
+co2.means <- eco2 %>% full_join(aco2) %>%
   group_by(co2.treat, chamber) %>%
   summarize(co2.ppm = mean(co2.meas)) %>%
   ungroup(chamber) %>%
   summarize(co2.mean = mean(co2.ppm),
             co2.se = sd(co2.ppm)/sqrt(6))
-  
-  
-  
+
+
 ## Some light analyses while I construct actual chamber environment script
 par.chamber <- data.frame(percent = seq(0, 100, 10),
                           ch1 = c(0, 107, 222, 327, 443, 552, 615, 705, 877, 1004, 1225),
@@ -232,47 +130,11 @@ par.chamber <- data.frame(percent = seq(0, 100, 10),
                           ch5 = c(0, 112, 233, 344, 466, 581, 649, 744, 926, 1061, 1295),
                           ch6 = c(0, 107, 222, 328, 444, 552, 616, 706, 879, 1006, 1228))
 
-
-
-
-
-
-par.chamber_long <- par.chamber %>%
-  pivot_longer(cols = ch1:ch6, names_to = "chamber", values_to = "par")
-
-
-
-
-
-ggplot(data = subset(par.chamber_long, percent < 50), aes(x = percent, y = par)) +
-  geom_point(aes(color = chamber), size = 3) +
-  geom_smooth(method = 'lm', se = TRUE) +
-  stat_poly_eq(parse=T, aes(label = ..eq.label..), formula=y~x)
-
-ggplot(data = subset(par.chamber_long, percent > 50), aes(x = percent, y = log(par))) +
-  geom_point(aes(color = chamber), size = 3) +
-  geom_smooth(method = 'lm', se = TRUE) +
-  stat_poly_eq(parse=T, aes(label = ..eq.label..), formula=y~x)
-
-
-par.lm.0to50percent <- lm(par ~ percent, data = subset(par.chamber_long, percent < 50))
-par.lm.50to100percent <- lm(log(par) ~ percent, data = subset(par.chamber_long, percent > 50))
-
-emmeans(par.lm.0to50percent, ~percent, at = list(percent = c(0, 25, 50)))
-emmeans(par.lm.50to100percent, ~percent, type = "response",
-        at = list(percent = c(75, 100)))
-
-ch1.par %>% full_join(ch2.par) %>% full_join(ch3.par) %>% 
-  full_join(ch4.par) %>% full_join(ch5.par) %>% full_join(ch6.par) %>%
-  group_by(percent) %>%
-  summarize(par.mean = mean(emmean, na.rm = TRUE),
-            par.sd = sd(emmean, na.rm = TRUE))
-
-# Mean and standard deviation for maximum PAR
+# Mean and standard deviation for maximum PAR averaged across chambers
 mean(as.numeric(par.chamber[11, c(2:7)]))
 sd(as.numeric(par.chamber[11, c(2:7)]))
 
-## Daily 16:8 light availability during day
+## Daily 16:8 light availability during day.
 day.par <- data.frame(chamber = seq(1,6,1), 
                       day.par = c(((11.45*25 - 20.5455)*1.5 + 
                                      (11.45*50 - 20.5455)*1.5 + 
@@ -299,6 +161,7 @@ day.par <- data.frame(chamber = seq(1,6,1),
                                      (11.4864*75 -20.8636)*1.5 +
                                      (1228*11.5))/16))
 
+## Mean and standard deviation of daily PAR values across chambers
 mean(day.par$day.par)
 sd(day.par$day.par)
   
